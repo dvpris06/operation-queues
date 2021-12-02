188413ce97daab8f8b858868c6febb1cf063bcb5
Dmitry Prisyazhnyuk <dprisyaz@ru.estee.com>
2021-12-02 15:09:12 +0300

    Initial Commit

---
+++ OperationQueueTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OperationQueueTask
+//
+//  Created by Dmitry Prisyazhnyuk on 02.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
